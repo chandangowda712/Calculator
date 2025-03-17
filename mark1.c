ec9bf8affb4aa8da683dc9a0f284c48d7854ef44
Chandan <chandan@gmail.com>
2025-03-17 16:48:46 +0530

    added mark1

---
+++ mark1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    // Input operator
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    // Input two numbers
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Perform calculation
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error! Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+    
+    return 0;
+}
+
